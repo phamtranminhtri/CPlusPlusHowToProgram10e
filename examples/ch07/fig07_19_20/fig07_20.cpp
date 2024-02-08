857e0644cadbb0f95305120b746230695c86f393
phamtranminhtri <tri.phamtranminh@hcmut.edu.vn>
2024-02-08 07:52:55 +0700

    change fig07_20 two dimensional std::array (initalize error)

--- examples/ch07/fig07_19_20/fig07_20.cpp
+++ examples/ch07/fig07_19_20/fig07_20.cpp
@@ -5,22 +5,34 @@
 using namespace std;
 
 int main() {
-   // two-dimensional array of student grades
-   array<array<int, GradeBook::tests>, GradeBook::students> grades{
-      {87, 96, 70},
-      {68, 87, 90},
-      {94, 100, 90},
-      {100, 81, 82},
-      {83, 65, 85},
-      {78, 87, 65},
-      {85, 75, 83},
-      {91, 94, 100},
-      {76, 72, 84},
-      {87, 93, 73}};
-   
-   GradeBook myGradeBook("CS101 Introduction to C++ Programming", grades);
-   myGradeBook.displayMessage();
-   myGradeBook.processGrades();
+	// two-dimensional array of student grades
+	array<array<int, GradeBook::tests>, GradeBook::students> grades{ /*1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,5,5,5*/
+	   87, 96, 70,
+	   68, 87, 90,
+	   94, 100, 90,
+	   100, 81, 82,
+	   83, 65, 85,
+	   78, 87, 65,
+	   85, 75, 83,
+	   91, 94, 100,
+	   76, 72, 84,
+	   87, 93, 73
+	};
+
+	array<array<int, GradeBook::column>, GradeBook::row> real = { {
+		{1,2,3,4},
+		{5,6,7,8}
+	} };
+
+	for (int i{ 0 }; i < 2; i++) {
+		for (int j{ 0 }; j < 4; j++) {
+			cout << real[i][j];
+		}
+	}
+
+	GradeBook myGradeBook("CS101 Introduction to C++ Programming", grades);
+	myGradeBook.displayMessage();
+	myGradeBook.processGrades();
 }
 
 
